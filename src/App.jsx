d16e9f99106cbc17944320bee59793ab9be56395
Nasir <hmdnasir6206@gmail.com>
2024-03-05 13:12:32 +0530

    developed Navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+
+
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarRadioItem, MenubarSeparator, MenubarShortcut, MenubarTrigger } from "./ui/menubar";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <div className="flex bg-white justify-between px-6 py-4">
+      <div className="w-20">
+        <img src={logo} alt="" className="w-full h-full bg-white"/>
+      </div>
+      <Menubar className='max-sm:hidden border-none' >
+        <MenubarMenu>
+          <MenubarTrigger>Crypto Taxes</MenubarTrigger>
+          <MenubarTrigger>Free Tools</MenubarTrigger>
+          <MenubarTrigger>Resourse Center</MenubarTrigger>
+          <Button className='bg-blue-700 text-white font-bold' >Get Started</Button>
+        </MenubarMenu>
+      </Menubar>
+    </div>
+  );
+};
+
+export default Navbar;
